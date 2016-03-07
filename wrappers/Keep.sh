87158af17e611476cebe2401114568ac1d24ce44
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2016-03-07 20:30:34 +0100

    Add dwm wrappers to the repo

---
+++ wrappers/Keep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+surf 'https://keep.google.com' &>/dev/null &
+# CPU eager app removed
+# google-chrome --profile-directory=Default --app-id=hmjkmjkepdijhoojdojkdfohbdgmmhki &
---
+++ wrappers/RapidMiner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/Applicazioni/RapidMiner/rapidminer-studio/RapidMiner-Studio.sh &
