31df90d392cfbc1b34522ea441db6bf4e35f0b18
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2019-07-10 16:53:20 +0200

    Update dwm-statusbar to use unicode chars

--- dwm-statusbar.sh
+++ dwm-statusbar.sh
@@ -6,6 +6,20 @@ vpn_status () {
 	[ "$vpnc_pid" ] && echo up || echo down
 }
 
+interval() {
+	local start_date="$(date -u -d "${1}" +"%s")"
+	local final_date="$(date -u -d "${2}" +"%s")"
+	date -u -d "0 ${final_date} sec -${start_date} sec" +"%T"
+}
+
+pomodoro_time="$(atq | head -n1 | cut -d' ' -f4)"
+if [[ -z $pomodoro_time ]]; then
+	POMO='-'
+else
+	current_time="$(date +%T)"
+	POMO="$(interval "${current_time}" "${pomodoro_time}")"
+fi
+
 DBOX="$(dropbox status | head -n1 | cut -b1-3)"
 if [[ "${DBOX}" == 'Dro' ]]; then
 	DBOX='-'
@@ -18,11 +32,11 @@ WLAN="$(nmcli device wifi list | sed -rn '1d; /^[*]/ s/ {2,}/;/gp' | awk -F';' '
 VPN="$(vpn_status)"
 #VOL="$(amixer get Master | grep -oP 'off(?=\])|\d+%(?=.*\[on\])')"
 VOL="$(pactl list sinks | perl -ne 'local $/; my $stdin = <>; print "$1" if ($stdin =~ /N[ao]me:\h+alsa_output.pci.*analog(?:.(?!\n\n))*?Mut[eo]:\h+(?:off|no).*?Volume:.*?(\d+%)/s)')"
-[[ ! $VOL ]] && VOL='off'
+[[ ! $VOL ]] && VOL='🔇' || VOL="🔊 ${VOL}"
 #VOL="$(amixer get Master | egrep -o '[0-9]+%')"
 #VOL="$(amixer get Master | awk '/[0-9]+%/ { gsub(/[][]/,""); printf("%s",$4)  }')"
 TEMP="$(grep -o '^[0-9][0-9]' /sys/class/thermal/thermal_zone0/temp)"
-DATE="$(date '+%A, %d.%m.%Y %H:%M')"
+DATE="$(date '+%a, %d.%m.%Y %H:%M')"
 LOAD=$(echo "$(cut -d' ' -f1 /proc/loadavg)*100" | bc | sed 's/\.[0-9]*//')
 FREE_DATA=$(free -m | grep Mem | tr -s ' ')
 CURRENT=$(echo "${FREE_DATA}" | cut -f3 -d' ')
@@ -33,13 +47,16 @@ HDD="$(df -lh | awk '{if ($6 == "/") { print $5 }}')"
 PLAYER=$(dwmp --bar)
 if [[ $PLAYER == mpd ]]; then PROGRESS="$(mpc status | grep -oP '\d+:\d+/\d+:\d+')"; PLAYER="${PLAYER} ${PROGRESS}"; fi
 # Combine data
-HEAD="WL: $WLAN"
-[[ $VPN == up ]] && HEAD="$HEAD | VPN: $VPN"
-HEAD="$HEAD | DBOX: $DBOX | CPU: ${CPU}% | MEM: ${MEM}% | LOAD: ${LOAD}% | HDD: $HDD | T: ${TEMP}C"
-TAIL="VOL: $VOL | PL: $PLAYER | $DATE"
+HEAD=" 📶 $WLAN"
+separator='   '
+[[ $POMO != - ]] && HEAD="${HEAD}${separator}🍅 ${POMO}"
+[[ $VPN == up ]] && HEAD="${HEAD}${separator}🔒 ${VPN}"
+[[ $DBOX != - ]] && HEAD="${HEAD}${separator}🎁 ${DBOX}"
+HEAD="${HEAD}${separator}⛋ ${CPU}%${separator}⛼ ${MEM}%${separator}⛃ ${LOAD}%${separator}💽 ${HDD}${separator}🌡️ ${TEMP}C"
+TAIL="${VOL}"; [[ $PLAYER != - ]] && TAIL="${TAIL}${separator}► ${PLAYER}"; TAIL="${TAIL}${separator}🗓️  ${DATE}"
 if acpi -a | grep off-line > /dev/null; then
 	BAT="$(acpi -b | cut -d' ' -f4 | tr -d '%,')"
-	xsetroot -name "${HEAD} | BAT: ${BAT}% | ${TAIL}"
+	xsetroot -name "${HEAD}${separator}🔋 ${BAT}%${separator}${TAIL}"
 	# Ask to suspend if lower than 10%
 	if [[ $BAT -le 10 ]]; then
 		xsetroot -name "LOW BATTERY! ${BAT}%"
@@ -47,5 +64,5 @@ if acpi -a | grep off-line > /dev/null; then
 		systemctl suspend
 	fi
 else
-	xsetroot -name "${HEAD} | ${TAIL}"
+	xsetroot -name "${HEAD}${separator}${TAIL}"
 fi
